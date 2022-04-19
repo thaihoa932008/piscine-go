f62d0ac9a852cc421dc6c8be68c16d8a5a4906c2
thaihoa932008 <62609771+thaihoa932008@users.noreply.github.com>
2022-04-19 10:07:27 +0300

    Update map.go

--- piscine_go/piscine-go/map.go
+++ piscine_go/piscine-go/map.go
@@ -1,17 +1,12 @@
-package main
-
-import "fmt"
+package piscine
 
 func Map(f func(int) bool, a []int) []bool {
 	// you create a var to store the slices of bool
 	var result []bool
+	// you make a range loop to go through every value of the slice of a
 	for _, boolval := range a {
+		// the result go through the function of the loop and add the bool value to the result
 		result = append(result, f(boolval))
 	}
-}
-
-func main() {
-	a := []int{1, 2, 3, 4, 5, 6}
-	result := Map(IsPrime.a)
-	fmt.Println(result)
+	return result
 }
